6c50f995edb3076b058d8e2901c6548011665ed7
AntonMelnychuk <antonmelnychuk1@gmail.com>
2023-01-23 19:18:24 +0100

    Add home page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,17 @@
+import { Header } from "./common/components/header/header.component";
+import { MenuList } from "./modules/menu/components/menu-list/menu-list.component";
+import pizzaMenu from "./mocks/pizza.json"
+import { Footer } from "./common/components/footer/footer.components";
 
 export const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-violet-600">🍕PizzaStack</h1>
+    <div>
+      <Header />
+      <div className="mx-12 mb-24">
+        <MenuList items={pizzaMenu} />
+      </div>
+      <Footer />
+    </div>
   );
 }
 
