2ce0eb492da48983313e15a9c9f5df0652903910
AntonMelnychuk <antonmelnychuk1@gmail.com>
2023-01-28 18:02:57 +0100

    Change admin to admin-register

---
+++ serverless/netlify/functions/admin-register.ts
@@ -0,0 +1,54 @@
+import { Handler } from "@netlify/functions";
+import { GraphQLClient } from "graphql-request";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import { getSdk } from "../common/sdk";
+
+interface AdminRegisterInput {
+  username: string;
+  password: string;
+}
+
+const handler: Handler = async (event, context) => {
+  const { body, headers } = event;
+
+  if(!headers["x-pizzastack-secret-key"] || headers["x-pizzastack-secret-key"] !== "mypizzastacksecretkey") {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({
+        message: "'x-pizzastack-secret-key' is missing or value is invalid",
+      }),
+    };
+  }
+  const input: AdminRegisterInput = JSON.parse(body!).input.admin;
+  const sdk = getSdk(new GraphQLClient("http://localhost:8080/v1/graphql"));
+
+  const password = crypto.pbkdf2Sync(input.password, "mysaltsecret", 1000, 64, "sha512").toString("hex");
+
+  const data = await sdk.InsertAdmin(
+    {
+    username: input.username,
+    password: password,
+    }
+  );
+
+  const accessToken = jwt.sign(
+    {
+    "https://hasura.io/jwt/claims": 
+      {
+      "x-hasura-allowed-roles": ["admin"],
+      "x-hasura-default-role": "admin",
+      "x-hasura-user-id": data.insert_admin_one?.id,
+      },
+    },
+    'myjwtsecret'
+  );
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ accessToken: accessToken }),
+    
+  };
+};
+
+export { handler };
